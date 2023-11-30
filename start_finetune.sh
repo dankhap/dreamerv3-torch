daab9c76b12d3d6da263fe981e05b0d09ab2879c
Daniel Khapun <daniel.khapun@gmail.com>
2023-11-30 11:55:52 +0200

    fix runner

--- start_finetune.sh
+++ start_finetune.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/bash
 #
-obs_type=${1}
-domain=${2}
-task=${3}
-ratio=${4}
+obs_type="pixel"
+domain=${1}
+task=${2}
+ratio=${3}
 
-# print input args for debug
-echo "obs_type: ${obs_type}"
-echo "domain: ${domain}"
-echo "task: ${task}"
-echo "ratio: ${ratio}"
 
 if [ "$obs_type" == "pixel" ]; then
 	dmc_config="dmc_vision"
@@ -24,6 +19,17 @@ elif [ "$domain" == "quadruped" ]; then
 fi
 
 sdate=$(date '+%Y%m%d_%H%M%S')
+
+# print input args for debug
+echo "obs_type: ${obs_type}"
+echo "domain: ${domain}"
+echo "task: ${task}"
+echo "ratio: ${ratio}"
+echo "model_path: ${model_path}"
+echo "dmc_config: ${dmc_config}"
+echo "date: ${sdate}"
+
+
 
 sbatch --export=ALL,A="fine --configs ${dmc_config} --task dmc_${domain}_${task} --wandb_group dreamerv3_${domain}_${task} --wandb_name ${domain}_${task}_pixel_finetune_${ratio} --modeldir /code/dreamerv3-torch/logdir/dmc_walker_walk/23111624/ --logdir /code/dreamerv3-torch/logdir/dmc_walker_run/${sdate}/ --reward_off False --trunc_buffer 0 --expl_until 10 --train_ratio ${ratio} --start_finetuning True --steps 1e5 " ./dreamer_job_git.sh
 
